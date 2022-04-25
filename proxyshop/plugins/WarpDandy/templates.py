4860f561a77b04ee7c2cb5a3a4b7e5d78f8f9d94
myojin223 <64243555+myojin223@users.noreply.github.com>
2022-04-25 07:37:07 -0600

    Added new templates

--- proxyshop/plugins/WarpDandy/templates.py
+++ proxyshop/plugins/WarpDandy/templates.py
@@ -58,6 +58,7 @@ class LegendsTemplate (temp.NormalClassicTemplate):
 class PortalTemplate (temp.NormalClassicTemplate):
     """
      * Created by WarpDandy
+     * Not currently working
     """
     def template_file_name (self):
         return "WarpDandy/Portal"
@@ -148,6 +149,134 @@ class ClassicWhiteBorderTemplate (temp.NormalClassicTemplate):
     
     def template_suffix (self):
         return "Classic WB"
+    
+    # OPTIONAL
+    def __init__ (self, layout, file):
+        # DO STUFF
+        super().__init__(layout, file)
+    
+    # OPTIONAL
+    def enable_frame_layers (self):
+        # DO STUFF
+        super().enable_frame_layers()
+ 
+class NicknameSmallTemplate (temp.NormalTemplate):
+    """
+     * The showcase template first used on the Women's Day Secret Lair. The layer structure of this template and NormalTemplate are
+     * similar, but this template doesn't have any background layers, and a layer mask on the pinlines group needs to be enabled when
+     * the card is legendary.
+    """
+    def template_file_name (self):
+        return "WarpDandy/NicknameSmall"
+
+    def template_suffix (self):
+        return "Ikoria S"
+
+    def __init__ (self, layout, file):
+        # strip out reminder text for fullart
+        super().__init__(layout, file)
+
+    def enable_frame_layers (self):
+
+        # twins and pt box
+        psd.getLayer(self.layout.twins, con.layers['TWINS']).visible = True
+        if self.is_creature:
+            psd.getLayer(self.layout.twins, con.layers['PT_BOX']).visible = True
+
+        # pinlines
+        pinlines = psd.getLayerSet(con.layers['PINLINES_TEXTBOX'])
+        if self.is_land:
+            pinlines = psd.getLayerSet(con.layers['LAND_PINLINES_TEXTBOX'])
+        psd.getLayer(self.layout.pinlines, pinlines).visible = True
+
+        if self.is_legendary:
+            # legendary crown
+            psd.getLayer(self.layout.pinlines, con.layers['LEGENDARY_CROWN']).visible = True
+            app.activeDocument.activeLayer = pinlines
+            psd.enable_active_layer_mask()
+           
+
+
+class NicknameMediumTemplate (temp.NormalTemplate):
+    """
+     * The showcase template first used on the Women's Day Secret Lair. The layer structure of this template and NormalTemplate are
+     * similar, but this template doesn't have any background layers, and a layer mask on the pinlines group needs to be enabled when
+     * the card is legendary.
+    """
+    def template_file_name (self):
+        return "WarpDandy/NicknameMedium"
+
+    def template_suffix (self):
+        return "Ikoria M"
+
+    def __init__ (self, layout, file):
+        # strip out reminder text for fullart
+        super().__init__(layout, file)
+
+    def enable_frame_layers (self):
+
+        # twins and pt box
+        psd.getLayer(self.layout.twins, con.layers['TWINS']).visible = True
+        if self.is_creature:
+            psd.getLayer(self.layout.twins, con.layers['PT_BOX']).visible = True
+
+        # pinlines
+        pinlines = psd.getLayerSet(con.layers['PINLINES_TEXTBOX'])
+        if self.is_land:
+            pinlines = psd.getLayerSet(con.layers['LAND_PINLINES_TEXTBOX'])
+        psd.getLayer(self.layout.pinlines, pinlines).visible = True
+
+        if self.is_legendary:
+            # legendary crown
+            psd.getLayer(self.layout.pinlines, con.layers['LEGENDARY_CROWN']).visible = True
+            app.activeDocument.activeLayer = pinlines
+            psd.enable_active_layer_mask()
+            
+   
+class NicknameLargeTemplate (temp.NormalTemplate):
+    """
+     * The showcase template first used on the Women's Day Secret Lair. The layer structure of this template and NormalTemplate are
+     * similar, but this template doesn't have any background layers, and a layer mask on the pinlines group needs to be enabled when
+     * the card is legendary.
+    """
+    def template_file_name (self):
+        return "WarpDandy/NicknameLarge"
+
+    def template_suffix (self):
+        return "Ikoria L"
+
+    def __init__ (self, layout, file):
+        # strip out reminder text for fullart
+        super().__init__(layout, file)
+
+    def enable_frame_layers (self):
+
+        # twins and pt box
+        psd.getLayer(self.layout.twins, con.layers['TWINS']).visible = True
+        if self.is_creature:
+            psd.getLayer(self.layout.twins, con.layers['PT_BOX']).visible = True
+
+        # pinlines
+        pinlines = psd.getLayerSet(con.layers['PINLINES_TEXTBOX'])
+        if self.is_land:
+            pinlines = psd.getLayerSet(con.layers['LAND_PINLINES_TEXTBOX'])
+        psd.getLayer(self.layout.pinlines, pinlines).visible = True
+
+        if self.is_legendary:
+            # legendary crown
+            psd.getLayer(self.layout.pinlines, con.layers['LEGENDARY_CROWN']).visible = True
+            app.activeDocument.activeLayer = pinlines
+            psd.enable_active_layer_mask()
+            
+class GoldenAgeTemplate (temp.NormalFullartTemplate):
+    """
+     * Created by WarpDandy
+    """
+    def template_file_name (self):
+        return "WarpDandy/GoldenAge"
+    
+    def template_suffix (self):
+        return "Golden Age"
     
     # OPTIONAL
     def __init__ (self, layout, file):
